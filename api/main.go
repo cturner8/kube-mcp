76a126d60165abbcefc50d5baee6b86cdbcd732f
cameron turner <17177161+cturner8@users.noreply.github.com>
2025-11-22 15:30:52 +0000

    update main to connect to api and discover server version

--- api/main.go
+++ api/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"log"
+
+	"github.com/cturner8/kube-mcp/config"
+	"github.com/cturner8/kube-mcp/kubernetes"
 	"github.com/cturner8/kube-mcp/server"
 )
 
 func main() {
+	kubernetesApiClient := kubernetes.CreateKubernetesApiClient(*config.ServerConfig.OutOfCluster, *config.ServerConfig.Kubeconfig)
+
+	version, err := kubernetesApiClient.Discovery().ServerVersion()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Printf("Connected to Kubernetes API Server. Version %s.", version.String())
+
 	server.StartServer()
 }
