aa7b73319210f0bbcc57fb1c34da0ded2fb255d2
cameron turner <17177161+cturner8@users.noreply.github.com>
2025-12-23 18:49:48 +0000

    add configuration values for allowed and disallowed tools

--- api/config/cli.go
+++ api/config/cli.go
@@ -10,13 +10,15 @@ import (
 
 func getMcpServerCliFlags() McpServerUserConfig {
 	var (
-		host           = flag.String("host", os.Getenv("HOST"), "host to connect to/listen on")
-		port           = flag.String("port", os.Getenv("PORT"), "port number to connect to/listen on")
-		outOfCluster   = flag.Bool("out-of-cluster", false, "(optional) indicates the server is running outside of a Kubernetes cluster and should look for a kubeconfig file")
-		allowedOrigins = flag.String("allowed-origins", os.Getenv("ALLOWED_ORIGINS"), "(optional) comma-separated list of allowed CORS origins")
-		baseUrl        = flag.String("base-url", os.Getenv("BASE_URL"), "Base URL the application will be accessed from")
-		oidcIssuerUrl  = flag.String("oidc-issuer-url", os.Getenv("OIDC_ISSUER_URL"), "URL of the OIDC authentication provider")
-		oidcClientId   = flag.String("oidc-client-id", os.Getenv("OIDC_CLIENT_ID"), "ID of the OIDC Client to authenticate against")
+		host            = flag.String("host", os.Getenv("HOST"), "host to connect to/listen on")
+		port            = flag.String("port", os.Getenv("PORT"), "port number to connect to/listen on")
+		outOfCluster    = flag.Bool("out-of-cluster", false, "(optional) indicates the server is running outside of a Kubernetes cluster and should look for a kubeconfig file")
+		allowedOrigins  = flag.String("allowed-origins", os.Getenv("ALLOWED_ORIGINS"), "(optional) comma-separated list of allowed CORS origins")
+		baseUrl         = flag.String("base-url", os.Getenv("BASE_URL"), "Base URL the application will be accessed from")
+		oidcIssuerUrl   = flag.String("oidc-issuer-url", os.Getenv("OIDC_ISSUER_URL"), "URL of the OIDC authentication provider")
+		oidcClientId    = flag.String("oidc-client-id", os.Getenv("OIDC_CLIENT_ID"), "ID of the OIDC Client to authenticate against")
+		allowedTools    = flag.String("allowed-tools", os.Getenv("ALLOWED_TOOLS"), "(optional) comma-separated list of allowed tools")
+		disallowedTools = flag.String("disallowed-tools", os.Getenv("DISALLOWED_TOOLS"), "(optional) comma-separated list of disallowed tools")
 	)
 
 	// Attempt to resolve a local kubeconfig path.
@@ -31,13 +33,15 @@ func getMcpServerCliFlags() McpServerUserConfig {
 	flag.Parse()
 
 	return McpServerUserConfig{
-		Host:           *host,
-		Port:           *port,
-		OutOfCluster:   *outOfCluster,
-		Kubeconfig:     *kubeconfig,
-		AllowedOrigins: *allowedOrigins,
-		BaseURL:        *baseUrl,
-		OidcIssuerURL:  *oidcIssuerUrl,
-		OidcClientID:   *oidcClientId,
+		Host:            *host,
+		Port:            *port,
+		OutOfCluster:    *outOfCluster,
+		Kubeconfig:      *kubeconfig,
+		AllowedOrigins:  *allowedOrigins,
+		BaseURL:         *baseUrl,
+		OidcIssuerURL:   *oidcIssuerUrl,
+		OidcClientID:    *oidcClientId,
+		AllowedTools:    *allowedTools,
+		DisallowedTools: *disallowedTools,
 	}
 }
