240fc418e296bbc622c98c1c6a1132b89485367f
cameron turner <17177161+cturner8@users.noreply.github.com>
2025-12-23 18:50:49 +0000

    add tool utility function to check if a specific tool is active

--- api/tools/tools.go
+++ api/tools/tools.go
@@ -1,6 +1,8 @@
 package tools
 
 import (
+	"slices"
+
 	"github.com/cturner8/kube-mcp/config"
 	"github.com/cturner8/kube-mcp/kubernetes"
 
@@ -8,3 +10,25 @@ import (
 )
 
 var kubernetesApiClient *k8s.Clientset = kubernetes.CreateKubernetesApiClient(*config.ServerConfig.OutOfCluster, *config.ServerConfig.Kubeconfig)
+
+func IsToolAllowed(toolName string) bool {
+	allowedTools := config.ServerConfig.AllowedTools
+	disallowedTools := config.ServerConfig.DisallowedTools
+
+	// If no restrictions, all tools are allowed
+	if len(allowedTools) == 0 && len(disallowedTools) == 0 {
+		return true
+	}
+
+	// If allowed tools are specified, check tool has been allowed
+	if len(allowedTools) > 0 {
+		return slices.Contains(allowedTools, toolName)
+	}
+
+	// If disallowed tools are specified, check tool has not been disallowed
+	if len(disallowedTools) > 0 {
+		return !slices.Contains(disallowedTools, toolName)
+	}
+
+	return false
+}
